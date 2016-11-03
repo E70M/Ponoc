933e12e5d20a0e014273cf61eaa2e207811d48bc
E70M <Eitan@Eitans-MacBook-Pro.local>
2016-11-03 13:25:44 -0700

    Updated files
    Temporarily set contents of KeyboardInput as comments. Requires further
    testing.

--- src/KeyboardInput.java
+++ src/KeyboardInput.java
@@ -1,53 +1,64 @@
 /**
  * Created by domin on 11/3/2016.
  */
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-public class KeyboardInput implements KeyListener {
+/*import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener; */
+public class KeyboardInput /*implements KeyListener*/ {
+    /*
     private static final int KEY_COUNT = 256;
+
     private enum KeyState {
         RELEASED,
         PRESSED,
         ONCE
     }
+
     private boolean[] currentKeys = null;
     private KeyState[] keys = null;
+
     public KeyboardInput() {
-        currentKeys = new boolean[ KEY_COUNT ];
-        keys = new KeyState[ KEY_COUNT ];
-        for( int i = 0; i < KEY_COUNT; ++i ) {
-            keys[ i ] = KeyState.RELEASED;
+        currentKeys = new boolean[KEY_COUNT];
+        keys = new KeyState[KEY_COUNT];
+        for (int i = 0; i < KEY_COUNT; ++i) {
+            keys[i] = KeyState.RELEASED;
         }
     }
+
     public synchronized void poll() {
-        for( int i = 0; i < KEY_COUNT; ++i ) {
-            if( currentKeys[ i ] ) {
-                if( keys[ i ] == KeyState.RELEASED )
-                    keys[ i ] = KeyState.ONCE;
+        for (int i = 0; i < KEY_COUNT; ++i) {
+            if (currentKeys[i]) {
+                if (keys[i] == KeyState.RELEASED)
+                    keys[i] = KeyState.ONCE;
                 else
-                    keys[ i ] = KeyState.PRESSED;
+                    keys[i] = KeyState.PRESSED;
             } else {
-                keys[ i ] = KeyState.RELEASED;
+                keys[i] = KeyState.RELEASED;
             }
         }
     }
-    public boolean keyDown( int keyCode ) {
-        return keys[ keyCode ] == KeyState.ONCE ||
-                keys[ keyCode ] == KeyState.PRESSED;
+
+    public boolean keyDown(int keyCode) {
+        return keys[keyCode] == KeyState.ONCE ||
+                keys[keyCode] == KeyState.PRESSED;
     }
-    public boolean keyDownOnce( int keyCode ) {
-        return keys[ keyCode ] == KeyState.ONCE;
+
+    public boolean keyDownOnce(int keyCode) {
+        return keys[keyCode] == KeyState.ONCE;
     }
-    public synchronized void keyPressed( KeyEvent e ) {
+
+    public synchronized void keyPressed(KeyEvent e) {
         int keyCode = e.getKeyCode();
-        if( keyCode >= 0 && keyCode < KEY_COUNT ) {
-            currentKeys[ keyCode ] = true;
+        if (keyCode >= 0 && keyCode < KEY_COUNT) {
+            currentKeys[keyCode] = true;
         }
     }
-    public synchronized void keyReleased( KeyEvent e ) {
+
+    public synchronized void keyReleased(KeyEvent e) {
         int keyCode = e.getKeyCode();
-        if( keyCode >= 0 && keyCode < KEY_COUNT ) {
-            currentKeys[ keyCode ] = false;
+        if (keyCode >= 0 && keyCode < KEY_COUNT) {
+            currentKeys[keyCode] = false;
         }
 
     }
+    */
+}
