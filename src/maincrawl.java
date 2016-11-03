73ab28f01c49864b6bfc7b795192e5fc5c6a44bf
E70M <Eitan@eitans-mbp.sduhsd.lan>
2016-11-03 08:13:18 -0700

    Added files

---
+++ src/maincrawl.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Eitan on 11/3/16.
+ */
+import javafx.application.Application;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.scene.paint.*;
+import javafx.scene.image.*;
+import javafx.geometry.*;
+public class maincrawl extends Application {
+    public static void main(String[]args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+}
