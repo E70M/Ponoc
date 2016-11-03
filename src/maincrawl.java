dd38918309d3a13c7cab54aca7abadf7e120df6d
E70M <Eitan@Eitans-MacBook-Pro.local>
2016-11-03 13:55:47 -0700

    Added images and updated files

--- src/maincrawl.java
+++ src/maincrawl.java
@@ -21,20 +21,27 @@ public class maincrawl extends Application {
         window.setTitle("Ponoc");
         GridPane gridlock = new GridPane();
         gridlock.setPadding(GLOBAL_INSETS);
-        gridlock.setVgap(40);
-        gridlock.setHgap(132);
+        gridlock.setVgap(30);
+        gridlock.setHgap(110);
         Image welcome = new Image("title.jpg");
+        Image settingsgear = new Image("gear.png");
         ImageView welcomemat = new ImageView(welcome);
+        ImageView settingsmenu = new ImageView(settingsgear);
         Label welcomeLabel = new Label();
         welcomeLabel.setGraphic(welcomemat);
+        Button settingsButton = new Button();
         Button startButton = new Button("Start Game");
         gridlock.setConstraints(welcomeLabel, 2, 1);
         gridlock.setConstraints(startButton, 2, 5);
+        gridlock.setConstraints(settingsButton, 0, 0);
         gridlock.setHalignment(welcomeLabel, HPos.CENTER);
         gridlock.setHalignment(startButton, HPos.CENTER);
-        // gridlock.setGridLinesVisible(true); // Temporary
+        gridlock.setHalignment(settingsButton, HPos.CENTER);
+        gridlock.setGridLinesVisible(true); // Temporary
         startButton.setOnAction(e -> fight.fightscene("Ponoc", GLOBAL_INSETS, window));
-        gridlock.getChildren().addAll(welcomeLabel, startButton);
+        settingsButton.setOnAction(e -> System.out.println("Test settings"));
+        settingsButton.setGraphic(settingsmenu);
+        gridlock.getChildren().addAll(settingsButton, welcomeLabel, startButton);
         display = new Scene(gridlock, 1000, 500);
         window.setScene(display);
         window.setResizable(false);
