10e13cc2d0ed729c4c72b0096d62c968f95394d7
E70M <Eitan@eitans-mbp.sduhsd.lan>
2016-11-03 08:18:49 -0700

    Update

--- src/maincrawl.java
+++ src/maincrawl.java
@@ -10,11 +10,18 @@ import javafx.scene.paint.*;
 import javafx.scene.image.*;
 import javafx.geometry.*;
 public class maincrawl extends Application {
+    static final Insets GLOBAL_INSETS = new Insets(10, 10, 10, 10);
+    static Stage window; static Scene display;
     public static void main(String[]args) {
         launch(args);
     }
     @Override
     public void start(Stage primaryStage) throws Exception {
-        
+        window = primaryStage;
+        GridPane gridlock = new GridPane();
+        display = new Scene(gridlock, 600, 300);
+        window.setScene(display);
+        window.setResizable(false);
+        window.show();
     }
 }
