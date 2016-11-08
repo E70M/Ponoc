795ea9ac0bdd1e934b0e2509a9e0099320ab7a70
E70M <Eitan@Eitans-MBP.myronfamily.homenet.org>
2016-11-07 18:11:07 -0800

    Moved settings button to intro screen

--- src/introcrawl.java
+++ src/introcrawl.java
@@ -2,6 +2,8 @@ import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -12,14 +14,19 @@ public class introcrawl {
     public static void screen (final Insets param, Stage parentStage, Scene parentScene) {
         GridPane introGrid = new GridPane();
         introGrid.setPadding(param);
-        introGrid.setVgap(420);
-        introGrid.setHgap(460);
+        introGrid.setVgap(380);
+        introGrid.setHgap(415);
         Button continueButton = new Button("Continue");
+        Button settingsButton = new Button();
         continueButton.setOnAction(e -> fight.fightscene(param, parentStage, parentScene));
+        settingsButton.setOnAction(e -> settings.setup(param, parentStage, parentScene));
+        settingsButton.setGraphic(new ImageView(new Image("gear.png")));
         introGrid.setConstraints(continueButton, 1, 1);
+        introGrid.setConstraints(settingsButton, 0, 0);
         introGrid.setHalignment(continueButton, HPos.CENTER);
+        introGrid.setHalignment(settingsButton, HPos.CENTER);
         introGrid.setStyle("-fx-background-image: url('intro.png'); -fx-background-color: #062f5a;");
-        introGrid.getChildren().addAll(continueButton);
+        introGrid.getChildren().addAll(continueButton, settingsButton);
         introGrid.setGridLinesVisible(false);
         Scene display = new Scene(introGrid, 1000, 500);
         parentStage.setScene(display);
