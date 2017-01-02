7c0151af5e8ffc5c878f036c136483c07d0211ae
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2017-01-01 16:44:05 -0800

    fixed keybaord input
    keyboard input moved to fight class, keyboard input class deleted

--- src/fight.java
+++ src/fight.java
@@ -4,9 +4,14 @@ import javafx.scene.layout.*;
 import javafx.scene.control.*;
 import javafx.geometry.*;
 import javafx.fxml.*;
+
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class fight extends Game {
+public class fight implements KeyListener {
+    public boolean over = false, paused;
+    public static final int UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3;
+    public int direction = DOWN;
     /*
     public void setupfight(final Insets param, Stage parentStage, Scene parentScene) {
         try {
@@ -40,5 +45,40 @@ public class fight extends Game {
     } */
     public static void fightscene() {
         // Insert fxml
+    }
+    public void startGame() {
+        over = false;
+        paused = false;
+        direction = DOWN;
+    }
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int i = e.getKeyCode();
+        if ((i == KeyEvent.VK_A || i == KeyEvent.VK_LEFT) && direction != RIGHT) {
+            direction = LEFT;
+        }
+        if ((i == KeyEvent.VK_D || i == KeyEvent.VK_RIGHT) && direction != LEFT) {
+            direction = RIGHT;
+        }
+        if ((i == KeyEvent.VK_W || i == KeyEvent.VK_UP) && direction != DOWN) {
+            direction = UP;
+        }
+        if ((i == KeyEvent.VK_S || i == KeyEvent.VK_DOWN) && direction != UP) {
+            direction = DOWN;
+        }
+        if (i == KeyEvent.VK_SPACE) {
+            if (over) {
+                startGame();
+            }
+            else {
+                paused = !paused;
+            }
+        }
+    }
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
     }
 }
