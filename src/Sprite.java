5fd066c8696820aac3d8329b95f66b1ef37b08f1
E70M <Eitan@eitans-mbp.sduhsd.lan>
2017-01-05 08:40:54 -0800

    Added sprite class
    Fuck XML

---
+++ src/Sprite.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import javax.swing.ImageIcon;
+public class Sprite {
+    protected int x, y, width, height;
+    protected boolean vis;
+    protected Image image;
+    public Sprite(int x, int y) {
+        this.x = x;
+        this.y = y;
+        vis = true;
+    }
+    protected void loadImage(String imageName) {
+        ImageIcon ii = new ImageIcon(imageName);
+        image = ii.getImage();
+    }
+    protected void getImageDimensions() {
+        width = image.getWidth(null);
+        height = image.getHeight(null);
+    }
+    public Image getImage() {
+        return image;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public boolean isVisible() {
+        return vis;
+    }
+    public void setVisible(Boolean visible) {
+        vis = visible;
+    }
+}
