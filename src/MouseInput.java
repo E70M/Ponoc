a1c864776eec2740df93e47bdc375198301c1002
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2017-01-05 17:55:54 -0800

    added mouse input
    /*

---
+++ src/MouseInput.java
@@ -0,0 +1,15 @@
+import java.awt.event.*;
+public class MouseInput {
+    //boolean attack = false;
+    public void mouseClicked(MouseEvent arg0) {
+        //attack(true);
+    }
+    public void mouseExited(MouseEvent arg0) {
+    }
+    public void mouseEntered(MouseEvent arg0) {
+    }
+    public void mousePressed(MouseEvent arg0) {
+    }
+    public void mouseReleased(MouseEvent arg0) {
+    }
+}
