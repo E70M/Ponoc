e7b260abbb60ecfafc92505ffa9236dda9af285e
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2017-05-09 10:49:37 -0700

    deleted extraneoues stuffz
    /*

--- src/Hero.java
+++ src/Hero.java
@@ -8,65 +8,7 @@ public class Hero extends Sprite {
     private String[][] filename = new String[8][maxFrameCount];
     Hero(double x, double y) {
         super(x,y);
-        initFileNames();
-        initRunLeft();
-        initRunRight();
-        initSwordLeft();
-        initSwordRight();
-        initJumpLeft();
-        initJumpRight();
-        initJumpSwordLeft();
-        initJumpSwordRight();
-    }
-    void initFileNames() {
-        filename[0][0] = ""; //runLeft
-        filename[0][1] = "";
-        filename[0][2] = "";
-        filename[0][3] = "";
-        filename[0][4] = "";
-        filename[0][5] = "";
-        filename[1][0] = ""; //runRight
-        filename[1][1] = "";
-        filename[1][2] = "";
-        filename[1][3] = "";
-        filename[1][4] = "";
-        filename[1][5] = "";
-        filename[2][0] = ""; //swordLeft
-        filename[2][1] = "";
-        filename[2][2] = "";
-        filename[2][3] = "";
-        filename[2][4] = "";
-        filename[2][5] = "";
-        filename[3][0] = ""; //swordRight
-        filename[3][1] = "";
-        filename[3][2] = "";
-        filename[3][3] = "";
-        filename[3][4] = "";
-        filename[3][5] = "";
-        filename[4][0] = ""; //jumpLeft
-        filename[4][1] = "";
-        filename[4][2] = "";
-        filename[4][3] = "";
-        filename[4][4] = "";
-        filename[4][5] = "";
-        filename[5][0] = ""; //jumpRight
-        filename[5][1] = "";
-        filename[5][2] = "";
-        filename[5][3] = "";
-        filename[5][4] = "";
-        filename[5][5] = "";
-        filename[6][0] = ""; //jumpSwordLeft
-        filename[6][1] = "";
-        filename[6][2] = "";
-        filename[6][3] = "";
-        filename[6][4] = "";
-        filename[6][5] = "";
-        filename[7][0] = ""; //jumpSwordRight
-        filename[7][1] = "";
-        filename[7][2] = "";
-        filename[7][3] = "";
-        filename[7][4] = "";
-        filename[7][5] = "";
+    
     }
 
     void swingSword(boolean toggle, boolean right) {
