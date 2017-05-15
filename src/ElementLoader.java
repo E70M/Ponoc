cfcf2624a3657bad1a3b0b70e73a7d4ec73f1230
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2017-05-15 10:29:32 -0700

    kill me

--- src/ElementLoader.java
+++ src/ElementLoader.java
@@ -1,6 +1,9 @@
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import java.awt.*;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
-public class ElementLoader {
+public class ElementLoader implements MouseListener{
     private ArrayList<String> input = new ArrayList<>();
     public Image getAppIcon() {
         return new Image(ElementLoader.class.getResourceAsStream("Ponoc_Icon.png"));
@@ -33,11 +36,34 @@ public class ElementLoader {
         input.remove(code);
     }
     public boolean isContaining(String test) {
-        if(input.contains(test)) {
+        if (input.contains(test)) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
+    }
+    @Override
+    public void mouseClicked(java.awt.event.MouseEvent e) {
+        addToInput("X");
+    }
+
+    @Override
+    public void mousePressed(java.awt.event.MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(java.awt.event.MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(java.awt.event.MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(java.awt.event.MouseEvent e) {
+
     }
 }
