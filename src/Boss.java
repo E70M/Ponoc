3578efda02cfe6cc1d1584f6e53ec329ffa7a3e2
E70M <eitanmyron@outlook.com>
2017-05-23 11:28:04 -0700

    Added some boss functions

--- src/Boss.java
+++ src/Boss.java
@@ -1,6 +1,17 @@
 public class Boss extends Enemy {
-    private int dx, dy;
+    private int dx, dy, lives = 20;
     public Boss(double x, double y) {
         super(x, y);
+    }
+    public void removeLife(int n) {
+        lives -= n;
+    }
+    public boolean isDead() {
+        if(lives == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
